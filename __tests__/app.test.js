09aa2b54e02d2cc44cd9e4b1d04974d29eefe9be
Adam Robson <adamray312@gmail.com>
2022-11-04 14:31:59 -0500

    fix testing function for doggys table

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,34 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+const Dogs = require('../lib/models/DogModel');
 
-describe('backend-express-template routes', () => {
+describe('doggys routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  test('/doggys returns list of dogs and details',
+    async () => {
+      const res = await request(app).get('/doggys');
+      const data = await Dogs.getAll();
+      expect(res.body).toEqual(data);
+    });
+  test('/doggys/:id returns dog detail', async () => {
+    const res = await request(app).get('/doggys/1');
+    const vincent = {
+      id: '1',
+      name: 'Vincent',
+      age: 11,
+      type: 'Afghan Hound',
+      size: 'large',
+      snack: 'cheez-its',
+      song: 'ACDC Ride On',
+      singer: false
+    };
+    expect(res.body).toEqual(vincent);
   });
+
   afterAll(() => {
     pool.end();
   });
