29dabf6ff8870ba8cea9e5960d11cf27a370494e
Adam Robson <adamray312@gmail.com>
2022-11-04 12:28:38 -0500

    build out send table with second resource

--- sql/setup.sql
+++ sql/setup.sql
@@ -19,3 +19,23 @@ VALUES
 ('Judith', 9, 'Maltipoo', 'small', 'anchovies', 'Billie Holiday All Of Me', true),
 ('Janet', 3, 'Havanese', 'small', 'snickers', 'Talking Heads This Must Be the Place', false)
 
+DROP TABLE IF EXISTS cows;
+
+CREATE TABLE cows (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR NOT NULL,
+  age INT NOT NULL,
+  type VARCHAR NOT NULL,
+  alias VARCHAR NOT NULL,
+  color VARCHAR NOT NULL,
+  movie VARCHAR NOT NULL,
+  chill BOOLEAN NOT NULL
+);
+
+INSERT INTO cows (name, age, type, alias, color, movie, chill)
+VALUES
+('Herb', 17, 'Beefalo', 'the Nose', 'burgundy', 'The Talking Heads (Live): Stop Making Sense', true),
+('Lottie', 6, 'Hungarian Grey', '0900', 'grey', 'Jimminy Cricket Christmas', true),
+('Basil', 4, 'Florida Cracker Cattle', 'potato chip', 'fuchsia', 'The Air Up There', false),
+('Gregory', 14, 'Nguni Cattle', 'peg leg greg', 'pea green', 'Swiss Family Robinson (based on the book by Robert Louis Stevenson'), true),
+('Georgina', 3, 'Pustertaler Sprinzen', 'the Sweede', 'periwinkle', 'The Man with the Golden Gun', false)
